29a4a6cd3d86cc6ab80c8f53b2d0e42664fd05c7
juanpis <juanpablo012005@gmail.com>
2026-02-24 13:52:44 -0500

    commit segunda prueba pull request

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@
     sirve para leer variables de entorno (.env)
     y ponerlas disponibles en toda la aplicación NestJS de forma ordenada y segura.
 
-    asdfghjklñ{}
+    asdfghjklñ{}adddasdsadsa
 */
 import { registerAs } from "@nestjs/config"; 
 
